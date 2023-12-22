731043ebf5f1d561257fd4d8b870904e85b495f7
elimendo <emendoza8361@gmail.com>
2023-12-22 09:47:05 -0800

    final touches have been made

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,23 @@
 
+function generate(event) {
+    event.preventDefault()
+    const length = document.getElementById('length').value;
+    const numbers = document.getElementById('numbers').value;
+    const lowerCase = document.getElementById('lowercase').value;
+    const symbols = document.getElementById('symbols').value;
+    const upperCase = document.getElementById('uppercase').value;
+    console.log('numbers')
+   const password = generatePassword(parseInt(length), symbols, upperCase, lowerCase, numbers,)
 
-function generatePassword(length, symbols, upperCase, lowerCase, numbers, noDuplicates) {
+    document.getElementById('output').innerHTML = password
+};
+
+
+
+
+function generatePassword(length, symbols, upperCase, lowerCase, numbers,) {
 let characters = "";
+
 
 if (numbers) {
     characters +="0123456789"
@@ -20,4 +36,11 @@ if (symbols) {
 }
 
 let passwordArray = [];
+
+while(passwordArray.length < length){
+    const character = characters[Math.floor(Math.random() * characters.length)];
+
+    passwordArray.push(character)
+}
+return passwordArray.join('');
 }
